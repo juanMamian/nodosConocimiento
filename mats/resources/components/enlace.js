6f6715ea4511a6d3a850e2f0988176ec39e59d96
juanMamian <juancho.mamian@gmail.com>
2022-06-15 08:28:50 -0500

    primer commit

---
+++ mats/resources/components/enlace.js
@@ -0,0 +1,10 @@
+const enlaceComponent={
+    
+    props:{        
+        texto:String,
+        direccion:String,
+    },
+    computed:{        
+    },
+    template:'<div class="enlace-component"><div id="zonaCabecera"><img class="icono" src="http://192.168.1.100:3000/public/atlasConocimiento/iconos/iconoEnlace.svg" /><div class="texto">{{texto}}</div></div><a :href="direccion" target="_blank"><div class="boton">Visitar</div></a></div>',    
+}

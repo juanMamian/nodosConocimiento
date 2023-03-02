518ddc82b5b46eee5b3b2b095c22e6e4ed0c560a
juanMamian <juancho.mamian@gmail.com>
2023-03-02 08:04:34 -0500

    migrados in los recursos

--- mats/resources/components/recuadro.js
+++ mats/resources/components/recuadro.js
@@ -24,15 +24,15 @@ const recuadroComponent={
         },
         srcIcono(){
             if(this.descubrimiento){
-                return "http://192.168.1.100:3000/public/atlasConocimiento/iconos/circle-exclamation-solid.svg";
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/circle-exclamation-solid.svg";
             }
             else if(this.instruccionPointer){
-                return "http://192.168.1.100:3000/public/atlasConocimiento/iconos/handPointer.svg";
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/handPointer.svg";
             }
             else if(this.instruccionTeclado){
-                return "http://192.168.1.100:3000/public/atlasConocimiento/iconos/iconoInstruccionTeclado.svg";
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/iconoInstruccionTeclado.svg";
             }
-            return "http://192.168.1.100:3000/public/atlasConocimiento/iconos/bombillo.png";
+            return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/bombillo.png";
 
         }
     },
---
+++ recursos/components/recuadro.js
@@ -0,0 +1,40 @@
+const recuadroComponent={
+    
+    props:{
+        tipo:String,        
+    },
+    computed:{
+        datoPrevio(){
+            return this.tipo==='datoPrevio'
+        },
+        datoNuevo(){
+            return this.tipo==='datoNuevo'
+        },
+        descubrimiento(){
+            return this.tipo==='descubrimiento'
+        },
+                instruccion(){
+            return this.tipo==='instruccion'
+        },
+        instruccionPointer(){
+            return this.tipo==='instruccionPointer'
+        },
+        instruccionTeclado(){
+            return this.tipo==='instruccionTeclado'
+        },
+        srcIcono(){
+            if(this.descubrimiento){
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/iconoDescubrimiento.png";
+            }
+            else if(this.instruccionPointer){
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/handPointer.svg";
+            }
+            else if(this.instruccionTeclado){
+                return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/iconoInstruccionTeclado.svg";
+            }
+            return "http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/bombillo.png";
+
+        }
+    },
+    template:'<div class="recuadro" :class="{datoPrevio, datoNuevo, descubrimiento, instruccion, instruccionPointer, instruccionTeclado}"> <img class="iconoRecuadro" :src="srcIcono" /> <div class="textoRecuadro"> <slot></slot> </div> </div>',    
+}

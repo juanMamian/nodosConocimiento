477c3a0bf6a129a6c82747d1d1573cc162e3b26b
Juan Mamián <juancho.mamian@gmail.com>
2023-04-19 23:09:37 -0500

    avances en el nodo de operacion aritmetica de interpretación introductoria

--- mats/resources/components/enlace.js
+++ mats/resources/components/enlace.js
@@ -6,5 +6,5 @@ const enlaceComponent={
     },
     computed:{        
     },
-    template:'<div class="enlace-component"><div id="zonaCabecera"><img class="icono" src="http://192.168.1.100:3000/public/atlasConocimiento/recursos/iconos/iconoEnlace.svg" /><div class="texto">{{texto}}</div></div><a :href="direccion" target="_blank"><div class="boton">Visitar</div></a></div>',    
+    template:'<div class="enlace-component"><div id="zonaCabecera"><img class="icono" src="http://192.168.1.105:3000/public/atlasConocimiento/recursos/iconos/iconoEnlace.svg" /><div class="texto">{{texto}}</div></div><a :href="direccion" target="_blank"><div class="boton">Visitar</div></a></div>',    
 }

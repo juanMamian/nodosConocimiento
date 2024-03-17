a0c74b75862f2dff64c9212e975c68e41c2e8b6a
Juan Mamián <juancho.mamian@gmail.com>
2024-03-16 21:28:21 -0500

    creada la función truncar, para ser importada

--- recursos/funciones/utilsMats.js
+++ recursos/funciones/utilsMats.js
@@ -4,3 +4,10 @@ export const degToRad = function(deg) {
 export const radToDeg = function(rad) {
     return rad * 180 / Math.PI;
 }
+export const truncar = function(num, decimales) {
+    let versionTruncada = num.toFixed(decimales);
+    if (Number(versionTruncada.split(".")[1]) === 0) {
+        return num
+    }
+    return versionTruncada;
+}

72c57daa916fce3c33307dbe8af68afec0cd1944
Juan Mamián <juancho.mamian@gmail.com>
2024-03-20 19:33:33 -0500

    avances en la primera evaluación de triángulo rectángulo

--- recursos/composables/evaluacionNodo.js
+++ recursos/composables/evaluacionNodo.js
@@ -1,12 +1,13 @@
 import "https://pe-pe-pe.herokuapp.com/public/resources/libraries/vue.global.js"
 const { ref } = Vue;
 
-export const useEvaluacionNodo = function(addToGenerarReto, addToReiniciarReto) {
+export const useEvaluacionNodo = function(addToGenerarReto, addToReiniciarReto, recolectarRespuestaUsuario) {
     const versionReto = ref(0);
     const reto = ref(null);
     function reiniciarReto() {
         reto.value = null;
-        respuestaUsuarioCorrecta.value=null;
+        respuesta.value = null;
+        respuestaUsuarioCorrecta.value = null;
         if (addToReiniciarReto) {
             addToReiniciarReto();
         }
@@ -25,15 +26,32 @@ export const useEvaluacionNodo = function(addToGenerarReto, addToReiniciarReto) 
     const versionRespuestaUsuario = ref(0);
     const inputRespuestaUsuario = ref(null);
 
-    function evaluarRespuestaUsuario(numerica = false) {
-        if (!inputRespuestaUsuario.value) {
-            console.log(`No había input de respuesta usuario`);
+    function evaluarRespuestaUsuario(respuestaUsuario, numerica = false) {
+        if (!respuestaUsuario) {
+            if (recolectarRespuestaUsuario) {
+                respuestaUsuario = recolectarRespuestaUsuario();
+            }
+            else {
+                if (!inputRespuestaUsuario.value) {
+                    console.log(`No había input de respuesta usuario`);
+                    return;
+                }
+                respuestaUsuario = inputRespuestaUsuario.value.value.trim();
+            }
+        }
+        if (respuestaUsuario == null) {
+            console.log(`No hay respuesta usuario`);
             return;
         }
-        let respuestaUsuario = inputRespuestaUsuario.value.value.trim();
         if (numerica) {
             respuestaUsuario = Number(respuestaUsuario);
         }
+        respuestaUsuarioCorrecta.value = false;
+        if (respuestaUsuario === respuesta.value) {
+            respuestaUsuarioCorrecta.value = true;
+        }
+        versionRespuestaUsuario.value++;
+
     }
 
     return {

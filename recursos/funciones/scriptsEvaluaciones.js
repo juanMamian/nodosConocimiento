3bd09724a474ef419fe01ee3029a0b42f2251a62
Juan Mamián <juancho.mamian@gmail.com>
2026-03-04 22:40:22 -0500

    Mensaje de ajustar height

--- recursos/funciones/scriptsEvaluaciones.js
+++ recursos/funciones/scriptsEvaluaciones.js
@@ -1,3 +1,18 @@
+const sendHeight = () => {
+    console.log(`Sending height`);
+    window.parent.postMessage(
+        { type: 'resize', height: document.body.scrollHeight },
+        '*' // or specify parent origin for security
+    );
+};
+
+if (document.readyState === 'complete') {
+    sendHeight(); // already loaded, call immediately
+} else {
+    window.addEventListener('load', sendHeight);
+}
+window.addEventListener('resize', sendHeight);
+
 function anunciarRespuestaIncorrecta() {
     const anuncioIncorrecto = document.querySelector(".anuncioRespuestaIncorrecta");
     const anuncioCorrecto = document.querySelector(".anuncioRespuestaCorrecta");
